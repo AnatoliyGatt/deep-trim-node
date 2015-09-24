a3cfc6cc387c90242c4e269d6cf95753d6ab298c
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-24 10:04:17 +0200

    Replace " with ' in definitions of all strings in deep-trim.js tests

--- test/deep-trim.js
+++ test/deep-trim.js
@@ -1,114 +1,114 @@
-var assert = require("assert");
-var trim = require("../lib/deep-trim");
+var assert = require('assert');
+var trim = require('../lib/deep-trim');
 
-describe("deep-trim", function() {
-    describe("init", function() {
-        it("should trim all strings within object", function() {
+describe('deep-trim', function() {
+    describe('init', function() {
+        it('should trim all strings within object', function() {
             var person = {
-                name: "   John   ",
-                surname: "Johnson   "
+                name: '   John   ',
+                surname: 'Johnson   '
             };
 
             person = trim(person);
 
             var expectedPerson = {
-                name: "John",
-                surname: "Johnson"
+                name: 'John',
+                surname: 'Johnson'
             };
 
-            assert.deepEqual(person, expectedPerson, "object strings should be trimmed");
+            assert.deepEqual(person, expectedPerson, 'object strings should be trimmed');
         });
 
-        it("should trim all strings within object and its nested objects", function() {
+        it('should trim all strings within object and its nested objects', function() {
             var person = {
-                name: "   John   ",
-                surname: "Johnson   ",
+                name: '   John   ',
+                surname: 'Johnson   ',
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: " USA   "
-                }
-            };
-
-            person = trim(person);
-
-            var expectedPerson = {
-                name: "John",
-                surname: "Johnson",
-                address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: "USA"
-                }
-            };
-
-            assert.deepEqual(person, expectedPerson, "object and it's nested objects strings should be trimmed");
-        });
-
-        it("should trim all strings within object and its nested objects, ignoring all non-string property values", function() {
-            var person = {
-                name: "   John   ",
-                surname: "Johnson   ",
-                age: 26,
-                address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: " USA   "
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: ' USA   '
                 }
             };
 
             person = trim(person);
 
             var expectedPerson = {
-                name: "John",
-                surname: "Johnson",
-                age: 26,
+                name: 'John',
+                surname: 'Johnson',
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: "USA"
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: 'USA'
                 }
             };
 
-            assert.deepEqual(person, expectedPerson, "object and its nested objects strings should be trimmed, ignoring all non-string property values");
+            assert.deepEqual(person, expectedPerson, 'object and it\'s nested objects strings should be trimmed');
         });
 
-        it("should trim all strings within object and its nested objects, including nested arrays", function() {
+        it('should trim all strings within object and its nested objects, ignoring all non-string property values', function() {
             var person = {
-                name: "   John   ",
-                surname: "Johnson   ",
+                name: '   John   ',
+                surname: 'Johnson   ',
                 age: 26,
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: " USA   "
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: ' USA   '
+                }
+            };
+
+            person = trim(person);
+
+            var expectedPerson = {
+                name: 'John',
+                surname: 'Johnson',
+                age: 26,
+                address: {
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: 'USA'
+                }
+            };
+
+            assert.deepEqual(person, expectedPerson, 'object and its nested objects strings should be trimmed, ignoring all non-string property values');
+        });
+
+        it('should trim all strings within object and its nested objects, including nested arrays', function() {
+            var person = {
+                name: '   John   ',
+                surname: 'Johnson   ',
+                age: 26,
+                address: {
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: ' USA   '
                 },
                 vehicles: [
-                    "   BMW",
-                    "Ferrari    ",
-                    "Lamborghini"
+                    '   BMW',
+                    'Ferrari    ',
+                    'Lamborghini'
                 ]
             };
 
             person = trim(person);
 
             var expectedPerson = {
-                name: "John",
-                surname: "Johnson",
+                name: 'John',
+                surname: 'Johnson',
                 age: 26,
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: "USA"
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: 'USA'
                 },
                 vehicles: [
-                    "BMW",
-                    "Ferrari",
-                    "Lamborghini"
+                    'BMW',
+                    'Ferrari',
+                    'Lamborghini'
                 ]
             };
 
-            assert.deepEqual(person, expectedPerson, "object and its nested objects strings, including nested arrays strings, should be trimmed");
+            assert.deepEqual(person, expectedPerson, 'object and its nested objects strings, including nested arrays strings, should be trimmed');
         });
     });
 });
